405e388b9fa48ac4bf57728b39c6413b1a7199b8
Serg-Sanhzura <alphapro100@gmail.com>
2021-06-09 20:18:10 +0300

    Hello

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello world')
+s = 'Spam'
+# print(s.replace('pa', 'joke'))
+# print(s)
+# print(s.find('m'))
+#
+# print(len(s))
+# print(s[2:])
+# print(2 * s[0:3], s[-2])
+# print('Hello world')
+d = {"name": {'first': 'Jonn', 'last': 'Smith'},
+     'jobs': ['dev', 'mng'],
+     "age": 39}
+print(d)
